200aed50e98289273d6dde4768ad9990767e1629
Manuel Arredondo <manuel.arredondo@nuance.com>
2017-08-13 21:08:43 -0400

    initial commit

---
+++ Assets/ControllerEventHandler.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+public class ControllerEventHandler : MonoBehaviour {
+
+    public VRTK_ControllerEvents controllerEvents;
+
+	// Use this for initialization
+	void Start () {
+		
+        if(controllerEvents!=null)
+        {
+            controllerEvents.TriggerClicked += ControllerEvents_TriggerClicked;
+
+            controllerEvents.TriggerPressed += ControllerEvents_TriggerPressed;
+            controllerEvents.TriggerReleased += ControllerEvents_TriggerReleased;
+
+            controllerEvents.TriggerTouchStart += ControllerEvents_TriggerTouchStart;
+            controllerEvents.TriggerTouchEnd += ControllerEvents_TriggerTouchEnd;
+        }
+        
+	}
+
+    private void ControllerEvents_TriggerTouchEnd(object sender, ControllerInteractionEventArgs e)
+    {
+        Debug.Log("ControllerEvents_TriggerTouchEnd, buttonPressure: " + e.buttonPressure);
+    }
+
+    private void ControllerEvents_TriggerTouchStart(object sender, ControllerInteractionEventArgs e)
+    {
+        Debug.Log("ControllerEvents_TriggerTouchStart Button pressure: " + e.buttonPressure);
+    }
+
+    private void ControllerEvents_TriggerReleased(object sender, ControllerInteractionEventArgs e)
+    {
+        Debug.Log("ControllerEvents_TriggerReleased Button pressure: " + e.buttonPressure);
+    }
+
+    private void ControllerEvents_TriggerPressed(object sender, ControllerInteractionEventArgs e)
+    {
+        Debug.Log("ControllerEvents_TriggerPressed Button pressure: " + e.buttonPressure);
+    }
+
+    private void ControllerEvents_TriggerClicked(object sender, ControllerInteractionEventArgs e)
+    {
+        Debug.Log("ControllerEvents_TriggerClicked Button pressure: " + e.buttonPressure);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/External/VRTK-master/VRTK-master/THANK_YOU_CREDITS.md
@@ -0,0 +1,75 @@
+# Thank You Credits
+
+A big thank you to the supporters of VRTK via the VRTK Patreon page at https://www.patreon.com/vrtk
+
+### Sponsors
+
+[![image](https://user-images.githubusercontent.com/1029673/27898738-28f0e226-621f-11e7-9fdb-8618d85ba372.png)](https://www.realestate.com.au) [![image](https://user-images.githubusercontent.com/1029673/29113652-8fa11ca4-7ce9-11e7-9e33-88308af369b9.png)](http://alvios.com/)
+
+### Name List
+
+ * **realestate.co.au - Luke Chadwick** *[Professional Sponsor]*
+ * **Alvios Game Studio - Blueteak** *[Sponsor]*
+ * Mark Bradley
+ * TreeFortress Games
+ * BinaryLegend 
+ * Eric 
+ * Tuukka Takala
+ * Mcdoogleh 
+ * Matt Ostgard
+ * cognitiveVR 
+ * Adam Salmi
+ * Tanner Ochel
+ * Agatha Yu
+ * Aldis Sipolins
+ * Michael Hurley
+ * Don MacSween
+ * Carl Wolsey
+ * Stephen Eisenhauer
+ * Daniel Shimmyo
+ * Chris Ross
+ * Jedrzej Jonasz
+ * Jonathan Linowes
+ * Tim Lobes
+ * Chris Tuminello
+ * Michael Angel
+ * Thomas Kildren
+ * Devon Grandahl
+ * Victor Lew
+ * Jeff Canavan
+ * adam awesomeface
+ * Eric Vander Wal
+ * VR Game Development Mini-Degree by Zenva
+ * Grayson Deitering
+ * mori 
+ * TigerLily 
+ * Roland 
+ * Hgulgen
+ * Michal VRoblewski
+ * Chris Kirby
+ * Trevor Gibbons
+ * ManBearPig 
+ * Kenneth Lemieux
+ * Joey Parr
+ * Axel dabee
+ * braden 
+ * Andy Baker
+ * Tim Foster
+ * Ben McNelly
+ * Ian McNab
+ * David Erosa
+ * Juho Salonen
+ * Casual Immersions Entertainment
+ * Jarrell Norwood
+ * Steve Weintraut
+ * Stephen Elkins
+ * David 
+ * Matthew Boynton
+ * Bartek Tułodziecki
+ * Kyle Mausser
+ * Niklas Dennerståhl
+ * Luke Pierson
+ * Balazs Faludi
+ * Will Lingard
+ * Joe Ferguson
+ * Adam Tannir
